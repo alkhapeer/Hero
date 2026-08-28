923b0a025c2f6610b30cb832e33a3d41ed503b2b
Rawaj <156031987+alkhapeer@users.noreply.github.com>
2026-08-29 00:42:14 +0300

    Update cache version from v3 to v4

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
-const CACHE='hero-academy-v3';
+const CACHE='hero-academy-v4';
 
 const CORE=[
   './',
