d38922d29e759875d4a6730833a3cf86e8230663
Rawaj <156031987+alkhapeer@users.noreply.github.com>
2026-08-28 12:19:53 +0300

    Change cache version to 'hero-academy-v3'
    Updated cache version from 'hero-academy-v2' to 'hero-academy-v3'.

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE='hero-academy-v2';
-const CORE=['./','./index.html','./manifest.webmanifest','./css/style.css','./js/app.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
